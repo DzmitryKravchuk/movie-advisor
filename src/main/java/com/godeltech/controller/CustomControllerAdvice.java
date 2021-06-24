6417141f5fe609c91d10b842a94ed6a20fbea32b
DzmitryKravchuk <dzmitry.kravchuk@gmail.com>
2021-06-24 10:09:36 +0300

    error handling

---
+++ src/main/java/com/godeltech/controller/CustomControllerAdvice.java
@@ -0,0 +1,41 @@
+package com.godeltech.controller;
+
+import com.godeltech.exception.MovieUserEvaluationPersistenceException;
+import com.godeltech.exception.NotUniqueLoginException;
+import com.godeltech.exception.PasswordIncorrectException;
+import com.godeltech.exception.ResourceNotFoundException;
+import com.godeltech.exception.UpdateNotMatchIdException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public final class CustomControllerAdvice {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(final RuntimeException e) {
+        log.error(e.getMessage(), e);
+        return e.getMessage();
+    }
+
+    @ExceptionHandler({UpdateNotMatchIdException.class,
+            NotUniqueLoginException.class, PasswordIncorrectException.class,
+            MovieUserEvaluationPersistenceException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handleBadRequestException(final RuntimeException e) {
+        log.error(e.getMessage(), e);
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleUncaughtException(final Exception e) {
+        log.error("Something went wrong", e);
+        return "Something went wrong";
+    }
+
+}
